45e6ba8a5421ee35c442a16829e18ac66e4e7d3e
Myroslava <kozlenkomiroslava@gmail.com>
2026-08-17 12:41:51 +0300

    Initial version without half of main section and footer. Only header and welcome input

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector("#submit").onclick = () => {
+    window.location.href="https://jobs.abstractvc.com/jobs";
+};
+
+document.querySelector("#signIn").onclick = () => {
+    window.location.href="https://www.abstract.com/people/";
+};
+
